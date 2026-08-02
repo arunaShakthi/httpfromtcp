9d11d3a66215dbb7abb9be5247c9fa371bcec928
arunaShakthi <arunashakthi002@gmail.com>
2026-08-02 19:37:41 +0530

    refactor: replace line reading logic with dedicated request parsing module in tcplistener

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,47 +1,14 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"github.com/arunaShakthi/httpfromtcp/internal/request"
 )
 
 const port = ":42069"
-
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
-	go func() {
-		defer f.Close()
-		defer close(lines)
-		currentLineContents := ""
-		for {
-			b := make([]byte, 8, 8)
-			n, err := f.Read(b)
-			if err != nil {
-				if currentLineContents != "" {
-					lines <- currentLineContents
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				return
-			}
-			str := string(b[:n])
-			parts := strings.Split(str, "\n")
-			for i := 0; i < len(parts)-1; i++ {
-				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
-				currentLineContents = ""
-			}
-			currentLineContents += parts[len(parts)-1]
-		}
-	}()
-	return lines
-}
 
 func main() {
 	listener, err := net.Listen("tcp", port)
@@ -58,11 +25,20 @@ func main() {
 		}
 		fmt.Println("Accepted connection from", conn.RemoteAddr())
 
-		linesChan := getLinesChannel(conn)
-
-		for line := range linesChan {
-			fmt.Println(line)
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Printf("error parsing request: %s\n", err)
+			conn.Close()
+			fmt.Println("Connection to", conn.RemoteAddr(), "closed")
+			continue
 		}
+
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %s\n", req.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", req.RequestLine.HttpVersion)
+
+		conn.Close()
 		fmt.Println("Connection to", conn.RemoteAddr(), "closed")
 	}
 }
